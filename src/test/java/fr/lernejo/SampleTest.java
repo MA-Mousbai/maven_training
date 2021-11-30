6253c0978c26dc9406e850b672eda9fbb60dc325
MA-Mousbai <mousbai@et.esiea.fr>
2021-11-30 18:49:27 +0100

    Add test to match 100% coverage

---
+++ src/test/java/fr/lernejo/SampleTest.java
@@ -0,0 +1,24 @@
+package fr.lernejo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class SampleTest {
+
+    @Test
+    void op() {
+        assertEquals(7, new Sample().op(Sample.Operation.ADD, 3, 4));
+        assertEquals(12, new Sample().op(Sample.Operation.MULT, 3, 4));
+    }
+
+    @Test
+    void fact() {
+        assertEquals(6, new Sample().fact(3));
+        assertEquals(720, new Sample().fact(6));
+        assertThrows(IllegalArgumentException.class,() -> new Sample().fact(-1));
+
+    }
+
+}
